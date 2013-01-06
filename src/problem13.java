6a596bd7f0742ddbb24bbac8787ad4210390b4fa
Kentaro Kuribayashi <kentarok@gmail.com>
2013-01-07 08:45:42 +0900

    Added problem1.3 and 1.4

---
+++ src/problem13.java
@@ -0,0 +1,14 @@
+// 問題1.3
+// 2つの文字列が与えられたとき、片方がもう片方の並び換えになっているかどうかを決定するメソッドを書いてください。
+
+public class Problem13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
