61375cd0898e398633d6af145cb40daacd190b8f
Kentaro Kuribayashi <kentarok@gmail.com>
2013-01-24 00:05:40 +0900

    Added problem 2.6

---
+++ src/Problem26.java
@@ -0,0 +1,21 @@
+// 問題 2.6
+// 循環する連結リストが与えられたとき、循環する部分の最初のノードを返すアルゴリズムを書いてください。
+//
+// 定義
+// 循環を含むリスト: 連結リストAではループを作るために、リスト内のノードの次へのポインタが以前に出現したノードを指している
+//
+// 例
+// 入力: A -> B -> C -> D -> E -> C (最初と同じもの)
+// 出力: C
+
+public class Problem26 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
